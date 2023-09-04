154d8b2c4eba73000943835c057bfd902a0af7a3
rian <mohammad.rian.ap@gmail.com>
2023-09-04 22:05:16 +0700

    separate struct task and scheduler

--- freqscheduler.go
+++ freqscheduler.go
@@ -18,7 +18,7 @@ func countToSleep() {
 	}
 }
 
-type Scheduler struct {
+type Task struct {
 	StartMs         int64
 	PrevFrameMs     int64
 	Interval        int64
@@ -27,11 +27,7 @@ type Scheduler struct {
 	RemainToFrameMs int64
 }
 
-type ISchedule interface {
-	Schedule()
-}
-
-func (s *Scheduler) Schedule() {
+func (s *Task) UpdateTiming() {
 	nowMs := time.Now().UnixMilli()
 	nowRelativeMs := nowMs - s.StartMs
 	if nowRelativeMs >= s.NextFrameMs {
@@ -43,7 +39,7 @@ func (s *Scheduler) Schedule() {
 	}
 }
 
-func (s *Scheduler) Schedule2() {
+func (s *Task) UpdateTiming2() {
 	nowMs := time.Now().UnixMilli()
 	var iterEllapsedMs int64
 	if s.PrevIterMs == 0 {
@@ -61,7 +57,7 @@ func (s *Scheduler) Schedule2() {
 	}
 }
 
-func (s *Scheduler) Schedule3() {
+func (s *Task) UpdateTiming3() int64 {
 	nowMs := time.Now().UnixMilli()
 	if s.NextFrameMs == 0 {
 		s.NextFrameMs = nowMs
@@ -72,19 +68,53 @@ func (s *Scheduler) Schedule3() {
 		overtimeMs %= s.Interval
 		s.NextFrameMs = nowMs + s.Interval - overtimeMs
 	}
+	return s.NextFrameMs
+}
+
+type Worker struct {
+	busy bool
+}
+
+type Scheduler struct {
+	Tasks   []Task
+	Workers []Worker
+}
+
+const MaxUint64 = ^uint64(0)
+const MaxInt64 = int64(MaxUint64 >> 1)
+
+func (s *Scheduler) Loop() {
+	for {
+		minNextMs := MaxInt64
+		for i := range s.Tasks {
+			nextMs := s.Tasks[i].UpdateTiming3()
+			if nextMs < minNextMs {
+				minNextMs = nextMs
+			}
+		}
+		nowMs := time.Now().UnixMilli()
+		remainMs := minNextMs - nowMs
+		if remainMs > 2 {
+			fmt.Printf("sleep %v ms\n", remainMs-2)
+			time.Sleep(time.Duration(remainMs-2) * time.Millisecond)
+		}
+	}
 }
 
 func main() {
 	fmt.Println("freqscheduler")
-	s := Scheduler{
+	s := Scheduler{}
+	s.Tasks = append(s.Tasks, Task{
 		StartMs:     time.Now().UnixMilli(),
 		PrevFrameMs: int64(0),
 		Interval:    int64(500),
 		NextFrameMs: int64(0),
-	}
-	for {
-		//s.Schedule()
-		s.Schedule3()
-		countToSleep()
-	}
+	})
+	s.Tasks = append(s.Tasks, Task{
+		StartMs:     time.Now().UnixMilli(),
+		PrevFrameMs: int64(0),
+		Interval:    int64(1000),
+		NextFrameMs: int64(0),
+	})
+	s.Loop()
 }
